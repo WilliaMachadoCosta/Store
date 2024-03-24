da1a3725d065111c15771fa4b3aab83ecceb8a6b
williammachadocosta <williammachadocosta.trabalho@gmail.com>
2024-03-24 12:54:39 -0300

    class validate

---
+++ Store.Shared/ProductValidator.cs
@@ -0,0 +1,17 @@
+﻿namespace Store.Shared
+{
+    public static class ProductValidator
+    {
+        public static void ValidateName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("The product name must be provided.", nameof(name));
+        }
+
+        public static void ValidateValue(decimal value)
+        {
+            if (value <= 0)
+                throw new ArgumentException("The product value must be greater than zero.", nameof(value));
+        }
+    }
+}
