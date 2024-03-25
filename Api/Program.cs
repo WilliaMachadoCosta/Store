101e6dd31c3a8fd2cc0e5ab5d3f33d09bffb245f
williammachadocosta <williammachadocosta.trabalho@gmail.com>
2024-03-25 08:54:54 -0300

    test pipeline

--- Api/Program.cs
+++ Api/Program.cs
@@ -54,11 +54,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-var devClient = "http://localhost:4200";
-app.UseCors(x => x
-.AllowAnyOrigin()
-.AllowAnyMethod()
-.AllowAnyHeader().WithOrigins(devClient));
+//var devClient = "http://localhost:4200";
+//app.UseCors(x => x
+//.AllowAnyOrigin()
+//.AllowAnyMethod()
+//.AllowAnyHeader().WithOrigins(devClient));
 
 app.UseHttpsRedirection();
 
