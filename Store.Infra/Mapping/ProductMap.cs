f85bb28eca968e67491c46935311f23ed730f5fa
williammachadocosta <williammachadocosta.trabalho@gmail.com>
2024-03-25 12:18:21 -0300

    Separating Seed from Configuration

--- Store.Infra/Mapping/ProductMap.cs
+++ Store.Infra/Mapping/ProductMap.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Store.Domain.Entities;
+using Store.Infra.SeedData;
 
 namespace Store.Infra.Mapping
 {
@@ -32,33 +33,7 @@ namespace Store.Infra.Mapping
 
             DateTime yesterday = DateTime.UtcNow.AddDays(-1);
 
-            builder.HasData(
-                new Product("COPO QUENCHER CITRON TIE DYE", "COPO QUENCHER - STANLEY HIDRATE A SUA MELHOR VERSÃO", "image1", 315.00m, 20)
-                {
-                    LastUpdated = DateTime.UtcNow,
-                    CreatedDate = yesterday
-                },
-                new Product("COPO TERMICO EVERYDAY STANLEY", "como o próprio nome já denuncia, o Copo Térmico Everyday Stanley 296ml é o seu companheiro", "image2", 185.00m, 30)
-                {
-                    LastUpdated = DateTime.UtcNow,
-                    CreatedDate = yesterday
-                },
-                new Product("CANECA TERMICA DE CERVEJA STANLEY", "Nós da Stanley valorizamos muito os momentos de descontração", "image3", 220.00m, 10)
-                {
-                    LastUpdated = DateTime.UtcNow,
-                    CreatedDate = DateTime.UtcNow
-                },
-                new Product("KIT GARRAFA TERMICA CLASSIC ", "Tem kit especial para os amantes de chimarrão!", "image4", 380.00m, 5)
-                {
-                    LastUpdated = DateTime.UtcNow,
-                    CreatedDate = DateTime.UtcNow
-                },
-                new Product("COPO TERMICO EVERYDAY STANLEY | 296ML", "O modelo do copo térmico Stanley é ideal para brindar com um vinho, gim, drink bem refrescante, suco", "image5", 185.00m, 3)
-                {
-                    LastUpdated = DateTime.UtcNow,
-                    CreatedDate = DateTime.UtcNow
-                }
-            );
+            builder.HasData(ProductSeedData.Seed());
         }
     }
 }
